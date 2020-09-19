e342fa6e73d8313daad79925661ffd01ff5b4471
otayfuroglu <tayfuroglu@gmail.com>
2020-09-19 14:50:58 +0300

    TypeError: unhashable type: 'list' have been debuged

--- src/schnetpack/md/calculators/basic_calculators.py
+++ src/schnetpack/md/calculators/basic_calculators.py
@@ -165,7 +165,8 @@ class MDCalculator:
         Args:
             system (schnetpack.md.System): System object containing current state of the simulation.
         """
-        forces = self.results[self.force_handle]
+        #forces = self.results[self.force_handle]
+        forces = self.results[self.force_handle[0]] #edited by omert
         system.forces = (
             forces.view(system.n_replicas, system.n_molecules, system.max_n_atoms, 3)
             * self.force_conversion
